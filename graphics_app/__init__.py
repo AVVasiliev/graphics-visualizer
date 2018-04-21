3f91fbf15f5da250026242bb3eefc593313daeee
a.vasiliev <a.vasiliev@logistictech.ru>
2018-04-21 12:27:34 +0300

    Createing config for download files and saving images

--- graphics_app/__init__.py
+++ graphics_app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+import os
 
+
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/static'
+IMAGES_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/static/images'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER + '/datasets'
 
 from graphics_app import routes
