6ec36a23bb0cc740b34b483645dc5420bc225900
a.vasiliev <a.vasiliev@logistictech.ru>
2018-04-21 13:11:51 +0300

    Configs for uploading files

--- graphics_app/__init__.py
+++ graphics_app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 import os
 
 
-UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/static'
-IMAGES_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/static/images'
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+IMAGES_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'images')
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER + '/datasets'
+app.config['UPLOAD_FOLDER'] = os.path.join(UPLOAD_FOLDER, 'datasets')
 
 from graphics_app import routes
